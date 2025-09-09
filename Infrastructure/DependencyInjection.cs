237968dd231de1abbac673a55fa84439ead44b0d
Makata <makata2025@gmail.com>
2025-09-10 02:42:38 +0800

    cleanup code

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -3,11 +3,6 @@ using Google.Cloud.Firestore;
 using Infrastructure.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure
 {
